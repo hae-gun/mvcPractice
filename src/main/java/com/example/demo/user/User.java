69ecca06621d09e0bb0cc37ce03498e9eb3e0e5b
Hae-gun <newchk610@gmail.com>
2021-07-28 00:22:22 +0900

    feat: 익셉션 처리 및 다국어 처리

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -3,12 +3,16 @@ package com.example.demo.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
+    @Size(min = 2, message = "Name 은 두글자 이상 입력해 주세요.")
     private String name;
+    @Past
     private Date joinDate;
 }
